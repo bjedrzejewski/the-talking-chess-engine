9c3635dba34e1be08ebdf7a42565583bf09ca426
Bartosz Jedrzejewski <bjedrzejewski@users.noreply.github.com>
2016-12-23 23:19:45 +0000

    Adding the empty Spring Boot project

---
+++ src/main/java/com/bjedrzejewski/talkingchess/TheTalkingChessEngineApplication.java
@@ -0,0 +1,12 @@
+package com.bjedrzejewski.talkingchess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheTalkingChessEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheTalkingChessEngineApplication.class, args);
+	}
+}
